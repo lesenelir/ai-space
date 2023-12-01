1be73eedd86e84425c89104b5d9edd7136cc0123
lesenelir <hi@lesenelir.me>
2023-12-02 02:41:23 +0800

    test: add /api/example to test getAuth()

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,38 @@
 import { authMiddleware } from "@clerk/nextjs"
 
-// The App is fully protected by Clerk.
+/**
+ *  This file will be run before every pages router and api routes.
+ *
+ *  middleware -> pages tsx
+ *  middleware -> pages api
+ *
+ *  when I use Postman to send request to api, it will run this file first.
+ */
+
 // This example protects all routes including api/trpc routes.
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware.
 export default authMiddleware({
   publicRoutes: ['/'],
-  async afterAuth(auth, req, res) {
-    // if (!auth.userId && !auth.isPublicRoute) {
-    //   return redirectToSignIn({ returnBackUrl: req.url })
-    // }
-
-    // console.log('🫣 auth.userId', auth.userId)
-    // console.log('😏 auth', auth)
-
-  }
+  // async afterAuth(auth, req, res) {
+  //   // When you are in Postman to debug api, it will run this code. So you should comment on it.
+  //   if (!auth.userId && !auth.isPublicRoute) {
+  //     return redirectToSignIn({ returnBackUrl: req.url })
+  //   }
+  //   console.log('11', auth.userId, auth.user)
+  //   const options = {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({ userId: auth.userId })
+  //   }
+  //   const response = await fetch('http:localhost:3000/api/auth', options)
+  //   const data = await response.json()
+  //   // get user data
+  //   // data.status & data.user
+  //   console.log(data)
+  // }
 })
 
 export const config = {
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,12 +1,26 @@
 import { UserButton } from '@clerk/nextjs'
 
 export default function ChatPage() {
+
+  const tt = async () => {
+    const Response = await fetch('/api/example', {
+      method: 'POST',
+    })
+
+    const data = await Response.json()
+
+    console.log(data)
+  }
+
   return (
     <>
       <header>
         <UserButton afterSignOutUrl={'/'}/>
       </header>
       Chat Page
+
+
+      <button className={'bg-blue-500'} onClick={tt}>Test</button>
     </>
   )
 }
