a7b7af1a85c48d612a1c846e6c61845b197e7e5e
lesenelir <hi@lesenelir.me>
2023-12-03 02:16:12 +0800

    fix: mobile support for Second Content Area

--- src/components/home/SecondContentArea.tsx
+++ src/components/home/SecondContentArea.tsx
@@ -8,7 +8,7 @@ export default function SecondContentArea() {
       <h1 className={'text-5xl font-semibold text-white my-6'}>Plans</h1>
 
       {/* Content */}
-      <div className={'flex flex-row gap-4 justify-between'}>
+      <div className={'flex md:flex-row max-md:flex-col gap-4 justify-between'}>
         {/* Left */}
         <PlansCard main={'free'} price={'$0 / month'} buttonText={'Try on Web'} />
 
