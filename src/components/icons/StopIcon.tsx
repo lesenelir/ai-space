aeac3a058ce6d7aaf40d7effec6127c2b879df16
lesenelir <hi@lesenelir.me>
2023-12-29 02:11:25 +0800

    chore: add stop icon

---
+++ src/components/icons/StopIcon.tsx
@@ -0,0 +1,25 @@
+import { type MouseEvent } from 'react'
+
+interface IProps {
+  width: number
+  height: number
+  className?: string
+  onClick?: (e: MouseEvent<HTMLElement, globalThis.MouseEvent>) => void
+}
+
+export default function StopIcon(props: IProps) {
+  const {width, height, className, onClick} = props
+
+  return (
+    <div className={className} onClick={onClick}>
+      <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-player-stop" width={width}
+           height={height} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round"
+           strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M5 5m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z"/>
+      </svg>
+</div>
+)
+}
+
+
